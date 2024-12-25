5a8d99bdddacc3c2362bd7d3c664e3fc37b3854e
Satya Janghu <janghu.satya@gmail.com>
2024-12-25 20:26:40 +0530

    finished chapter 2 - strings

--- 2-3.py
+++ 2-3.py
@@ -1,3 +1,22 @@
 name = "Satya"
 
 print(f"Hello, {name}! Merry Christmas!")
+
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+
+famous_person = "Yoda"
+
+print(f"Do or do not, there is no try. - {famous_person}")
+
+title = "\tChaudhary\n"
+print (title)
+
+print(title.lstrip())
+print(title.rstrip())
+print(title.strip())
+
+filename = "foo.bar"
+
+print(filename.removesuffix('.bar'))
